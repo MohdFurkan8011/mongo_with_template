bcf2055caef672bf5941ba36c27c80dac611cad2
MohdFurkan <mfs0786siwal@gmail.com>
2021-06-25 09:06:05 +0530

    Initial commit

---
+++ src/main/java/com/mongo/template/mongo_with_template/MongoWithTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.template.mongo_with_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWithTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWithTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongo/template/mongo_with_template/MongoWithTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.template.mongo_with_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoWithTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
